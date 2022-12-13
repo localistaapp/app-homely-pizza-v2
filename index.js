35128cc638343aa074b44612335435f2f66982a2
sampath <sampath.kumar@sixt.com>
2022-12-13 13:06:28 +0530

    homepage

--- index.js
+++ index.js
@@ -1253,15 +1253,7 @@ app.post('/createConfirmedOrder', function(req, res) {
                                  res.send("error");
                               } else {
                                 console.log(response);
-                                axios
-                                  .post('https://api.pushalert.co/rest/v1/send', 'title=Event Order%20Received&message=New%20Pizza%20Event&icon=https://www.slimcrust.com/rounded.png&url=https://www.slimcrust.com', {headers: {'Authorization': 'api_key=c0a692d5772f7c2b7642013d80439aea'}})
-                                  .then(res => {
-                                    console.log('Pushalert success: ', res);
-                                  })
-                                  .catch(error => {
-                                    console.log('Pushalert error: ', error);
-                                  });
-                                 res.send('{"orderId":"'+orderId+'", "whitelisted":true}');
+                                res.send('{"orderId":"'+orderId+'", "whitelisted":true}');
                               }
 
                             });
