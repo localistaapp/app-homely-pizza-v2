cfc669875b1911aaabcb1bc0f9e5c4d826d84320
Srishti S <srishti@Srishtis-MacBook-Air.local>
2023-10-04 18:50:25 +0530

    updated club ordering flow

--- index.js
+++ index.js
@@ -1618,7 +1618,7 @@ app.post('/store/web-order', function(req, res) {
                   userId = response.rows[0].id;
               }
 
-          client.query("INSERT INTO \"public\".\"online_order\"(name, mobile, address, delivery_pincode, delivery_schedule, delivery_timeslot, \"order\", price, user_id, store_id) VALUES('"+name+"', '"+mobile+"', '"+address+"', '"+pincode+"', '"+schedule+"', '"+slot+"', '"+items+"', '"+price+"', '"+userId+"', '"+storeId+"')",
+          client.query("INSERT INTO \"public\".\"online_order\"(name, mobile, address, delivery_pincode, delivery_schedule, delivery_timeslot, \"order\", price, user_id, store_id) VALUES('"+name+"', '"+mobile+"', '"+address+"', '"+pincode+"', '"+schedule+"', '"+slot+"', '"+items+"', '"+price+"', "+userId+", "+storeId+")",
                       [], (err, response) => {
                             if (err) {
                               console.log(err)
