41f5914b9442a7f816d0f7b3db6c40ac2998967b
Srishti S <srishti@Srishtis-MacBook-Air.local>
2024-11-16 13:41:46 +0530

    pushalert integration

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
+var cacheName = 'slimcrust-pwa';
+var filesToCache = [
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  
+});
 importScripts("https://cdn.pushalert.co/sw-61487_2.js");
