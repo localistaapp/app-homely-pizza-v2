dd0fa260066fd918e6bbdbaf0ac218c3d8bc1ba2
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-01-30 23:57:42 +0530

    Updated delivery details

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var axios = require('axios');
 var nodemailer = require("nodemailer");
 //var nodemailer = {createTransport: function(){}};
 var crypto = require('crypto');
+var cors = require('cors');
 var QRCode = require('qrcode');
 //var mergeImages = require('merge-images');
 var base64 = require('file-base64');
